03ef05da22ab276cb7ca1e4cf4f419eebad63286
Aya Harb <ayass3r@gmail.com>
2015-03-10 13:51:38 +0200

    Backend Updated

--- DU.c
+++ DU.c
@@ -1,94 +1,45 @@
-//
-//
-//
-//
-//
-//
-///
-//
-//
-/*
+
+#include <dirent.h> //Directory Entries Format
+#include <unistd.h> //All ffollowing 3 required for lstat system call
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <time.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <pwd.h>
+#include <grp.h>
+#include <stdbool.h>
 
 
-DO NOT MESS WITH UNTIL I GIVE YOU GUYS THE OKAY!! PLEASE!!!! 
+//Declaring FileType. Using typedef enum to prevent other types from being added
+typedef enum {Direct, File, Character, Block, SystemLink, Unknown} FileType
 
-I'M MESSING AROUND WITH ENVIRONMENTS SO THIS COULD PROBABLE JUST RUIN MY LAPTOP IF I TRY TO OPEN IT AND SOMETHING HAS BEEN MESSED UP!!!
-VIEW THE OTHER FILE, THE SAMPLE FILE TO GET THE FORMAT!!!!
-
-
-
-*/
-
-//
-//
-//
-//
-///
-//
-//
-//
-////
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+//Declaring File Statistics
+struct FileStat{
+  char Name[2048];
+  FileType Type;
+  long Inode;
+  long Link;
+  char User[64];
+  char Group[64];
+  long long Size;
+  long long TotalSize;
+  long long NumberOfBlocks;
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Prototypes
+int StatFile( char *Path, char *Name, struct FileStat *StatBuffer, int Flags);
+void Print(struct FileStat *Stats, int Flags);
+long long Directory(char *RDirectory, int flags);
+int HumanFormat(long long Number, char *Format);
 
 int main (int argc, char **argv)
 {
+  Du-> example.txt //Revise Enterprize
   char *Directory;
   char Path[2048];
   char HFormat[10];
@@ -103,47 +54,44 @@ int main (int argc, char **argv)
   struct FileStat StatBuffer;
   int Stat;
 
-  {
-    /* data */
-  };
 
   for(i = 1; i < argc; i++)
     if(argv[i][0] != '-')
       argv_count++;
-    while ((Option = getopt(argc, argv, "irbsdaAtloHhf")) != -1) {
+    while ((Option = getopt(argc, argv)) != -1) {
       switch (Option) {
         case 'i':
-          Flags |= FLAG_SHOW_INODE;
+          Flags |= 1;
           break;
         case 'r':
-          Flags |= FLAG_RECURSE_MASK;
+          Flags |= 2;
           break;
         case 'b':
-          Flags |= FLAG_SHOW_BLOCKS;
+          Flags |= 4;
           break;
         case 's':
-          Flags |= FLAG_SHOW_SIZE;
+          Flags |= 8;
           break;
         case 'd':
-          Flags |= FLAG_SHOW_DIRECTORY;
+          Flags |= 16;
           break;
         case 'a':
-          Flags |= FLAG_SHOW_ALL_FILES;
+          Flags |= 32;
           break;
         case 'A':
-          Flags |= FLAG_SHOW_ALL_ATTRS;
+          Flags |= 64;
           break;
         case 't':
-          Flags |= FLAG_SHOW_FILE_TYPE;
+          Flags |= 128;
           break;
         case 'l':
-          Flags |= FLAG_SHOW_LINKS;
+          Flags |= 256;
           break;   
         case 'o':
-          Flags |= FLAG_SHOW_OWNER;
+          Flags |= 512;
           break;
         case 'H':
-          Flags |= FLAG_SHOW_HUMAN_FORMAT;
+          Flags |= 1024;
           break;
         case 'h':
           printf("Disk Analyzer 0.1\n");
@@ -165,10 +113,10 @@ int main (int argc, char **argv)
           exit(0);
           break;
         case 'f':
-          Flags |= FLAG_ONLY_FILES_AND_DIRS;
+          Flags |= 2048;
           break;   
         default:
-           fprintf(stderr, "Invalid option %c, Usage: %s [-options] <path>\n", Option, argv[0]);
+           fprintf(stderr, "Invalid option %c, Usage: %s [-options] <Path>\n", Option, argv[0]);
            exit(EXIT_FAILURE);
       }
   }
@@ -204,7 +152,7 @@ int main (int argc, char **argv)
     if(DirInode->d_name[0] != '.' || (Flags & FLAG_SHOW_ALL_FILES)){
       
 
-      if(stat_file(Directory, DirInode->d_name, &StatBuffer, Flags) != 0){
+      if(StatFile(Directory, DirInode->d_name, &StatBuffer, Flags) != 0){
         sprintf(Path, "Stat error while opening %s%s", Directory, DirInode->d_name);
         perror(Path);
       }else{
@@ -225,20 +173,21 @@ int main (int argc, char **argv)
   closedir(DirectoryQT);  
   
   return 0;
-
 }
 
-int stat_file(char *path, char *Name, struct file_stat *StatBuffer, int Flags){
-  int fd;
-  struct Stat Buffer;
-  char Path[MAX_FULL_PATH_SIZE];
 
-  if (path[strlen(path) - 1] != '/')
+int StatFile( char *Path, char *Name, struct FileStat *StatBuffer, int Flags)
+{ 
+  int FileDirectory;
+  struct stat Buffer;
+  char Path[2048];
+
+  if (path[strlen(path) - 1] != '/')  //Add a missing / if absent
     strcat(path, "/");
 
-  sprintf(Path, "%s%s", path, Name);
+  sprintf(Path, "%s%s", Path, Name); 
 
-  if ((fd=open(Path, O_RDONLY)) < 0){
+  if ((FileDirectory=open(Path, O_RDONLY)) < 0){
      return -1;
   }
 
@@ -248,92 +197,92 @@ int stat_file(char *path, char *Name, struct file_stat *StatBuffer, int Flags){
 
  strcpy(StatBuffer->Name, Name);
  switch (Buffer.st_mode & S_IFMT) {
-   case S_IFBLK: StatBuffer->Type = BLOCK_DEVICE;   break;
-   case S_IFCHR: StatBuffer->Type = CHARACTER_DEVICE;  break;
-   case S_IFDIR: StatBuffer->Type = DIRECTORY;   break;
-   case S_IFIFO: StatBuffer->Type = FIFO_PIPE; break;
-   case S_IFLNK: StatBuffer->Type = SYMLINK;   break;
-   case S_IFREG: StatBuffer->Type = REGULAR_FILE;   break;
-   case S_IFSOCK: StatBuffer->Type = SOCKET;   break;
-   default:  StatBuffer->Type = UNKNOWN;  break;
+   case S_IFBLK: StatBuffer->Type = Block;   break;
+   case S_IFCHR: StatBuffer->Type = Character;  break;
+   case S_IFDIR: StatBuffer->Type = Direct;   break;
+   case S_IFLNK: StatBuffer->Type = SystemLink;   break;
+   case S_IFREG: StatBuffer->Type = File;   break;
+   default:  StatBuffer->Type = Unknown;  break;
  }
 
  StatBuffer->Inode = (long)Buffer.st_ino;
  StatBuffer->Link = (long) Buffer.st_nlink;
  strcpy(StatBuffer->User, getpwuid(Buffer.st_uid)->pw_name);
  strcpy(StatBuffer->Group, getgrgid(Buffer.st_gid)->gr_name);
- if (((!(Flags & FLAG_ONLY_FILES_AND_DIRS)) || ((Buffer.st_mode & S_IFMT) == S_IFDIR) || ((Buffer.st_mode & S_IFMT) == S_IFREG)) && !(Buffer.st_blocks == 0)){
+ 
+ if (((!(Flags & 2048)) || ((Buffer.st_mode & S_IFMT) == S_IFDIR) || ((Buffer.st_mode & S_IFMT) == S_IFREG)) && !(Buffer.st_blocks == 0)){
    StatBuffer->Size = (long long) Buffer.st_size;
    StatBuffer->TotalSize = (long long) Buffer.st_size;
  }else{
    StatBuffer->Size = 0;
    StatBuffer->TotalSize = (long long) 0;
  }
- if (((Flags & FLAG_RECURSE_MASK) || (Flags & FLAG_SHOW_ALL_ATTRS)) && StatBuffer->Type == DIRECTORY){
-   // printf("Recursing: %s\n", Path);
+ 
+ if (((Flags & 2) || (Flags & 64)) && StatBuffer->Type == Direct){
     StatBuffer->TotalSize += get_dir_size(Path, Flags);
  }
  
  StatBuffer->NumberOfBlocks = (long long) Buffer.st_blocks;
 
- return close(fd);
+ return close(FileDirectory);
 }
 
-void print_stat(struct file_stat *st, int Flags){
+void Print(struct FileStat *Stats, int Flags);
+{
   char h_format[10];
-  if(Flags & FLAG_SHOW_FILE_TYPE || Flags & FLAG_SHOW_ALL_ATTRS)
-    switch (st->Type) {
-     case BLOCK_DEVICE:  printf("b ");   break;
-     case CHARACTER_DEVICE:  printf("c ");   break;
-     case DIRECTORY:  printf("d ");   break;
-     case FIFO_PIPE:  printf("p ");   break;
-     case SYMLINK:  printf("l ");   break;
-     case REGULAR_FILE:  printf("f ");   break;
-     case SOCKET: printf("s ");   break;
+  if(Flags & 128 || Flags & 64)
+    switch (Stats->Type) {
+     case Block:  printf("b ");   break;
+     case Character:  printf("c ");   break;
+     case Direct:  printf("d ");   break;
+     case SystemLink:  printf("l ");   break;
+     case File:  printf("f ");   break;
      default:       printf("- ");   break;
    }
-   if(Flags & FLAG_SHOW_INODE || Flags & FLAG_SHOW_ALL_ATTRS)
-      printf("%8ld ", st->Inode);
-   if(Flags & FLAG_SHOW_LINKS || Flags & FLAG_SHOW_ALL_ATTRS)
-      printf("%8ld ", st->Link);
-   if(Flags & FLAG_SHOW_LINKS || Flags & FLAG_SHOW_ALL_ATTRS)
-      printf("%8ld  ", st->Link);
-   if(Flags & FLAG_SHOW_OWNER || Flags & FLAG_SHOW_ALL_ATTRS)
-      printf("%4s:%4s  ", st->User, st->Group);
-   if(Flags & FLAG_SHOW_SIZE || Flags & FLAG_SHOW_ALL_ATTRS){
-      if (Flags & FLAG_SHOW_HUMAN_FORMAT){
-        get_human_format(st->Size, h_format);
+   if(Flags & 1 || Flags & 64)
+      printf("%8ld ", Stats->Inode);
+   if(Flags & 256 || Flags & 64)
+      printf("%8ld ", Stats->Link);
+   if(Flags & 256 || Flags & 64)
+      printf("%8ld  ", Stats->Link);
+   if(Flags & 512 || Flags & 64)
+      printf("%4s:%4s  ", Stats->User, Stats->Group);
+   if(Flags & 8 || Flags & 64){
+      if (Flags & 1024){
+        get_human_format(Stats->Size, h_format);
         printf("%s ", h_format);
       }else
-        printf("%lldB  ", st->Size);
+        printf("%lldB  ", Stats->Size);
       
    }
-   if(Flags & FLAG_RECURSE_MASK || Flags & FLAG_SHOW_ALL_ATTRS){
-      if (Flags & FLAG_SHOW_HUMAN_FORMAT){
-        get_human_format(st->TotalSize, h_format);
+   if(Flags & 2 || F
+    lags & 64){
+      if (Flags & 1024){
+        get_human_format(Stats->TotalSize, h_format);
         printf("%s ", h_format);
       }else
-         printf("%lldB  ", st->TotalSize);
+         printf("%lldB  ", Stats->TotalSize);
       
    }
-   if(Flags & FLAG_SHOW_BLOCKS || Flags & FLAG_SHOW_ALL_ATTRS)
-      printf("%5lldb ", st->NumberOfBlocks);
-   printf("%s \n", st->Name);
+   if(Flags & 4 || Flags & 64)
+      printf("%5lldb ", Stats->NumberOfBlocks);
+   printf("%s \n", Stats->Name);
 }
-// proc root run sys tmp
-long long get_dir_size(char *r_dir, int Flags){
+
+long long Directory(char *RDirectory, int Flags)
+{
   DIR *DirectoryQT;
   struct dirent *DirInode;
-  struct Stat Buffer;
-  char Path[MAX_FULL_PATH_SIZE];
+  struct stat Buffer;
+  char Path[2048];
   long long TotalSize = 0;
 
-  if (r_dir[strlen(r_dir) - 1] != '/')
-    strcat(r_dir, "/");
+  if (RDirectory[strlen(RDirectory) - 1] != '/')
+    strcat(RDirectory, "/");
 
-  if((DirectoryQT = opendir(r_dir)) == NULL){
+  if((DirectoryQT = opendir(RDirectory)) == NULL){
     perror("opendir");
-    printf("%s\n", r_dir);
+    printf("%s\n", RDirectory);
     return 0;
   }
 
@@ -359,29 +308,33 @@ long long get_dir_size(char *r_dir, int Flags){
   return TotalSize;
 }
 
-int get_human_format(long long number, char *format){
-  if (number <= 1000){
-    return sprintf(format, "%lldB", number);
-  }else if (number <= 1000000){
-    return sprintf(format, "%1.1fKBs", (float)number / 1000.0);
-  }else if(number <= 1000000000){
-    return sprintf(format, "%1.1fMBs", (float)number / 1000000.0);
+int HumanFormat(long long Number, char *Format)
+{
+  if (Number <= 1000){
+    return sprintf(Format, "%lldB", Number);
+  }else if (Number <= 1000000){
+    return sprintf(Format, "%1.1fKBs", (float)Number / 1000.0);
+  }else if(Number <= 1000000000){
+    return sprintf(Format, "%1.1fMBs", (float)Number / 1000000.0);
   }else{
-    return sprintf(format, "%1.1fGBs", (float)number / 1000000000.0);
+    return sprintf(Format, "%1.1fGBs", (float)Number / 1000000000.0);
   }
 }
 
 
 
-
-struct FileStat{
-  char Name[2048];
-  FILE_TYPE Type;
-  long Inode;
-  long Link;
-  char User[64];
-  char Group[64];
-  long long Size;
-  long long TotalSize;
-  long long NumberOfBlocks;
-};
+/*
+Legend: FLAGS!!
+Flag Show INode = 1
+Flag Recurse Mask = 2
+Flag Show Blocks = 4
+Flag Show Size = 8
+Flag Show Directory = 16
+Flag Show All files = 32
+Flag Show All Attrs = 64
+Flag Show File Type = 128
+Flag Show Link = 256
+Flag Show Owner = 512
+Flag Show Human Format = 1024
+Flag Only Files and Directories  = 8
+*/
